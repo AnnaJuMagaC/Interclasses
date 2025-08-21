450225be0b0bb8dcadb9d7b0a28c42fe6a6f14d8
AnnaJuMagaC <anna.j.costa8@alunosenai.br>
2025-08-21 15:24:04 -0300

    Adicionar arquivos de projeto.

---
+++ Interclasses/Models/Jogador.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interclasses.Models
+{
+    public class Jogador
+    {
+        public int JogadorId { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome do Jogador é Obrigatório")]
+        [MaxLength(50, ErrorMessage = "O Nome do Jogador pode ter no máximo 50 caracteres.")]
+        [Display(Name = "Nome do Jogador")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Número do Jogador é Obrigatório")]
+        [Range(1, 999, ErrorMessage = "O Número do Jogador deve estar entre 1 e 999.")]
+        [Display(Name = "Número do Jogador")]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "O campo Posição é Obrigatório")]
+        [MaxLength(20, ErrorMessage = "A Posição pode ter no máximo 20 caracteres.")]
+        [Display(Name = "Posição do Jogador")]
+        public string? Posicao { get; set; }
+
+        // Relacionamento com Time
+        [Display(Name = "Time")]
+        public int TimeId { get; set; }
+        public Time? Time { get; set; }
+    }
+}
---
+++ Interclasses/Models/Time.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interclasses.Models
+{
+    public class Time
+    {
+        public int TimeId { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome do Time é Obrigatório")]
+        [MaxLength(50, ErrorMessage = "O Nome do Time pode ter no máximo 50 caracteres.")]
+        [Display(Name = "Nome do Time")]
+        public string? Nome { get; set; }
+
+        //Relacionamento entre Models (Tabelas no Banco de Dados)
+        [Display(Name = "Esporte")]
+        public int EsporteId { get; set; } // Vicular o campo de Id da tabela
+        public Esporte? Esporte { get; set; } // Vincular o objeto relacionado ao Id acima.
+    }
+}
